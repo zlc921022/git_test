dc88e0020227c959e109a6ba38277232cc02cedd
zlc <zlc921022@163.com>
2020-09-07 16:33:15 +0800

    ‘初始化完成提交’

---
+++ app/src/main/java/com/xiaochen/git_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xiaochen.git_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
