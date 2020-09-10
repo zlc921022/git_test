a09b02e95f825640851d695899a9c20061d565d3
zlc921022 <zlc921022@163.com>
2020-09-11 00:45:00 +0800

    9999

--- app/src/main/java/com/xiaochen/git_test/MainActivity.java
+++ app/src/main/java/com/xiaochen/git_test/MainActivity.java
@@ -10,7 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ///1
         ///2
+
+        ///3
+        ///4
+
     }
 }
