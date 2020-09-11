424cd51f12e93673f765d6fb150c6cdc7223395d
zhenglecheng <zle921022@163.com>
2020-09-10 17:09:45 -0700

    测试提交2

--- app/src/main/java/com/xiaochen/git_test/MainActivity.java
+++ app/src/main/java/com/xiaochen/git_test/MainActivity.java
@@ -11,10 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 1. 测试rebase1
-<<<<<<< HEAD
         // 1. 测试rebase2
         // 3. 测试rebase3
-=======
->>>>>>> b740c0c... 测试提交1
     }
 }
